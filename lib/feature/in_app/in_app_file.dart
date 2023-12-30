c5c3fcb9b8b889e59e02725f8823e965979dd4a9
Zeeshan Sheikh <zeeshansheikh921837@gmail.com>
2023-12-30 15:00:27 +0500

    billing permission added

---
+++ lib/feature/in_app/in_app_file.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class InAppScreen extends StatelessWidget {
+  const InAppScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('In App '),
+        centerTitle: true,
+      ),
+      body: const Column(
+        children: 
+        [
+          
+        ],
+      ),
+    );
+  }
+}
